ed6a7ab7e101c02981ec51b8afac50efe7867923
Joseph <54170160+dnjoe96@users.noreply.github.com>
2022-06-25 01:27:46 +0200

    Update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Module contains function to returns a function """
+from typing import List, Union, Tuple, Callable
+
+
+def element_length(lst: List[List[str]]) -> List[Tuple[str, int]]:
+    return [(i, len(i)) for i in lst]
