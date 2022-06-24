9e46207f263b08bb5dda0553098f2184cbdfd62c
Joseph <54170160+dnjoe96@users.noreply.github.com>
2022-06-25 01:41:50 +0200

    Update 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" Module containing function that returns list's first element or None"""
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """A function that returns each element replicated 
+    to factor list from lst"""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
