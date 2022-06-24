3cdc631096b5d5877d5518d0a7294ba4a7fb267c
Joseph <54170160+dnjoe96@users.noreply.github.com>
2022-06-24 23:53:23 +0200

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Module contains function to return sum of items in a list """
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ Functions return sum of items in a list """
+    return sum(input_list)
