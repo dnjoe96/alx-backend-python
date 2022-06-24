ab6c39685744064511f79ce670b60b9af65f8fbd
Joseph <54170160+dnjoe96@users.noreply.github.com>
2022-06-25 00:35:23 +0200

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Module contains function to returns a tuple """
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ Functions return a tuple """
+    return (k, float(v))
