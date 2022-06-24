4206fb41169c035db5bb7b6a10dbc220350911a6
Joseph <54170160+dnjoe96@users.noreply.github.com>
2022-06-25 00:28:04 +0200

    Update 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """ Module contains function to return sum of items in a list """
 from typing import Union, List
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ Functions return sum of items in a list """
     return sum(mxd_lst)
