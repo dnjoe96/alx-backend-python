5b54d2390e01109ab9f6da552eea52fc63d0a7c0
Joseph <54170160+dnjoe96@users.noreply.github.com>
2022-06-25 01:41:40 +0200

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Module containing function that returns list's first element or None"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ Function returns a list's first element or None"""
+    if lst:
+        return lst[0]
+    else:
+        return None
