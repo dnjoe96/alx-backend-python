fa1293e0a576dc21e9912c01e04d53e974a7bf15
Joseph <54170160+dnjoe96@users.noreply.github.com>
2022-06-25 01:18:35 +0200

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Module contains function to returns a function """
+from typing import Union, Tuple, Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ Functions return a function """
+    def innerFunc(number):
+        return multiplier * number
+    return innerFunc
