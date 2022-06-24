7e6a96515ec03378e7d60ea5db6217a8adf68838
Joseph <54170160+dnjoe96@users.noreply.github.com>
2022-06-25 01:41:45 +0200

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" Module containing function that returns list's first element or None"""
+from typing import Mapping, TypeVar, Union, Any
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None
+) -> Union[Any, T]:
+    """Return Value of a dct @ key or the default parameter"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
