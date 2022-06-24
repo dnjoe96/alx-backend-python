f76820dad6451df2403dbdc052b0209e361f0888
Joseph <54170160+dnjoe96@users.noreply.github.com>
2022-06-24 23:45:07 +0200

    Update 4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Module contains function to return string of a float """
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
