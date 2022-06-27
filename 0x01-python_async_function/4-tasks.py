83e74ae541f7f1081b28fdf0383ecf7e319a53ac
dnjoe96 <donjoedbest@gmail.com>
2022-06-27 21:39:41 +0100

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+""" Module for wait_n function """
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ FUnction returns list of wait time"""
+    respList = await asyncio.\
+        gather(*(task_wait_random(max_delay) for _ in range(n)))
+    return sorted(respList)
