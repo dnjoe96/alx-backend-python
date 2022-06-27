f71b98f6c911f9203ece64794303d0481b494951
dnjoe96 <donjoedbest@gmail.com>
2022-06-27 20:29:32 +0100

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+""" Module for wait_n function """
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ FUnction returns list of wait time"""
+    respList = await asyncio.\
+        gather(*(wait_random(max_delay) for _ in range(n)))
+    return sorted(respList)
