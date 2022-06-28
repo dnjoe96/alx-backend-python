254df444b16550b4c2c671c17eb7d706191aed71
dnjoe96 <donjoedbest@gmail.com>
2022-06-28 15:08:30 +0100

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ import random
 async def async_generator():
     """ Function to generate async delay"""
     for i in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
         # await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
