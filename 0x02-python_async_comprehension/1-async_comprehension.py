0e4f9777d72ea02d8e2bcb0d68021ffcad3b450e
dnjoe96 <donjoedbest@gmail.com>
2022-06-28 15:16:38 +0100

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+""" Module for async_generator function """
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """ The function returns a list of radom numbers from
+    async_generator function
+    """
+    return [num async for num in async_generator()]
